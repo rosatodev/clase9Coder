e5ee26a72406af9035fbdaf99dcfdc49820908a5
Rosato <rosato.dev@gmail.com>
2022-01-10 21:39:55 -0300

    Primera parte de la segunda parte de la clase, osea dsps del recreo
    Aca muestran eventos de teclas y te dejo este q esta buenisimo para las busquedas

---
+++ keys.js
@@ -0,0 +1,4 @@
+let input = document.getElementById("input");
+
+// Este re sirve para las busquedas, cada vez que se escribe algo hace algo, osea en ese caso una busqueda y te daria los cosos relacionados para que vos toques el que estas buscando
+input.onkeyup = () => console.log("Evento keyUp");
